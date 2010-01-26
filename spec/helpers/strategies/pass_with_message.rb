0e9c535084f9456fa1ffb9c6407b0eb3af06a5a3
Jonas Grimfelt <grimen@gmail.com>
2010-01-26 06:39:20 +0100

    Support for passing a custom message with Warden::Strategy::Base#success! as second optional parameter (more similar to Warden::Strategy::Base#fail!). Defaults to nil.

--- spec/helpers/strategies/pass_with_message.rb
+++ spec/helpers/strategies/pass_with_message.rb
@@ -1,7 +1,7 @@
-Warden::Strategies.add(:pass) do
+Warden::Strategies.add(:pass_with_message) do
   def authenticate!
     request.env['warden.spec.strategies'] ||= []
-    request.env['warden.spec.strategies'] << :pass
+    request.env['warden.spec.strategies'] << :pass_with_message
     success!("Valid User", "The Success Strategy Has Accepted You") unless scope == :failz
   end
 end
